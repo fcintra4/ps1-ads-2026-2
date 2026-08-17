8c7214f837143458f413eb0b8c22e5f5a6537bbe
maymayk <mayuniilkim@gmail.com>
2026-08-17 14:02:24 +0000

    (17/08) Criação e configuração do BD e primeira migration

--- api/src/main/java/br/edu/fatecfranca/api/controllers/HelloController.java
+++ api/src/main/java/br/edu/fatecfranca/api/controllers/HelloController.java
@@ -12,4 +12,9 @@ public class HelloController {
    public String hello() {
        return "API em funcionamento!";
    }
+   @GetMapping("/test")
+   public String test() {
+       return "Isto é um teste.";
+   }
+
 }
