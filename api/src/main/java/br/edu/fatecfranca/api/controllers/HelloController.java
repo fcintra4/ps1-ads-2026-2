472081b5feadb6af365293166ca450170b23013e
Lara Croft <laramorerariberorr5665@gmail.com>
2026-08-31 11:48:15 +0000

    (17/08) Criação e configuração do BD e primeira migration

--- api/src/main/java/br/edu/fatecfranca/api/controllers/HelloController.java
+++ api/src/main/java/br/edu/fatecfranca/api/controllers/HelloController.java
@@ -13,6 +13,9 @@ public class HelloController {
    public String hello() {
        return "API em funcionamento!";
    }
-
+      @GetMapping("/test")
+   public String test() {
+       return "Isto é um teste.";
+   }
 
 }
